3bd6ed6372fa88bc8fbc06dc6515ace406c9111d
Jessica Bushong <jessica.bushong08@gmail.com>
2020-12-21 15:20:34 -0600

    add service click event

--- src/Components/StudentView/StudentDashboard/AddServiceHours.tsx
+++ src/Components/StudentView/StudentDashboard/AddServiceHours.tsx
@@ -1,49 +1,45 @@
 import React, { Component } from "react";
 import Sitebar from "../../Sitebar/Sitebar";
 
-import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
 import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import {
-    Link
-   } from "react-router-dom";
-   import { Redirect } from "react-router-dom";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import { Link } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 
-   type AcceptedProps = {
-     setIsAdminFalse: any;
-     isAdmin:any;
-    sessionToken?: any;
-    backArrowToggle: any;
-    arrowHandler: any;
-    clearToken: any;
-    date: any;
-    typeOfService: any;
-    description: any;
-    hours:any;
-    status: any; 
-    studentUserId: any;
-    setDate: (e: any) => void;
+type AcceptedProps = {
+  setIsAdminFalse: any;
+  isAdmin: any;
+  sessionToken?: any;
+  backArrowToggle: any;
+  arrowHandler: any;
+  clearToken: any;
+  date: any;
+  typeOfService: any;
+  description: any;
+  hours: any;
+  status: any;
+  studentUserId: any;
+  setDate: (e: any) => void;
   setTypeOfService: (e: any) => void;
   setDescription: (e: any) => void;
   setHours: (e: any) => void;
   setStatus: (e: any) => void;
-  };
-class AddServiceHours extends React.Component <AcceptedProps, {}> {
-  componentDidMount(){
+};
+class AddServiceHours extends React.Component<AcceptedProps, {}> {
+  componentDidMount() {
     this.props.arrowHandler();
     this.props.setIsAdminFalse(false);
     if (!this.props.sessionToken) {
@@ -71,37 +67,36 @@ class AddServiceHours extends React.Component <AcceptedProps, {}> {
       headers: new Headers({
         "Content-Type": "application/json",
       }),
-    })
-      .then((response) => {
-        if (response.status === 200) {
-          console.log("Login was successful");
-        } else {
-          console.log("Login in failed");
-        }
-        return response.json();
-      })
+    }).then((response) => {
+      if (response.status === 200) {
+        console.log("Login was successful");
+      } else {
+        console.log("Login in failed");
+      }
+      return response.json();
+    });
   };
 
   render() {
     return (
       <div>
         <Sitebar
-        backArrowToggle={this.props.backArrowToggle}
-        arrowHandler={this.props.arrowHandler}
-        clearToken={this.props.clearToken}
-        sessionToken={this.props.sessionToken}
+          backArrowToggle={this.props.backArrowToggle}
+          arrowHandler={this.props.arrowHandler}
+          clearToken={this.props.clearToken}
+          sessionToken={this.props.sessionToken}
         />
-        <Container style={{paddingLeft:"40px", paddingRight:"40px"}}component="main" maxWidth="xs">
+        <Container
+          style={{ paddingLeft: "40px", paddingRight: "40px" }}
+          component="main"
+          maxWidth="xs"
+        >
           <CssBaseline />
           <div style={{ marginTop: "25px" }}>
-            <Typography
-           
-              component="h1"
-              variant="h5"
-            >
+            <Typography component="h1" variant="h5">
               Add Service
             </Typography>
-        
+
             <br></br>
             <br></br>
             <form onSubmit={this.handleSubmit} noValidate>
@@ -120,41 +115,78 @@ class AddServiceHours extends React.Component <AcceptedProps, {}> {
                 </Grid>
                 <Grid item xs={12} sm={6}></Grid>
                 <Grid item xs={12}>
-                  <FormControl style={{minWidth:160}} >
-                    <InputLabel id="demo-simple-select-label">Type of Service</InputLabel>
+                  <FormControl style={{ minWidth: 160 }}>
+                    <InputLabel id="demo-simple-select-label">
+                      Type of Service
+                    </InputLabel>
                     <Select
                       labelId="demo-simple-select-label"
                       id="demo-simple-select"
-                    //   value={age}
-                    //   onChange={handleChange}
+                      //   value={age}
+                      //   onChange={handleChange}
                     >
-                      <MenuItem value={10}>Tutoring</MenuItem>
-                      <MenuItem value={20}>Recycling</MenuItem>
-                      <MenuItem value={30}>NJHS Sponsored Event</MenuItem>
-                      <MenuItem value={30}>Other</MenuItem>
+                      <MenuItem
+                        value={"Tutoring"}
+                        onClick={(e) => {
+                          this.props.setTypeOfService("Tutoring");
+                          console.log(this.props.typeOfService);
+                        }}
+                      >
+                        Tutoring
+                      </MenuItem>
+                      <MenuItem value={"Recycling"}
+                          onClick={(e) => {
+                        this.props.setTypeOfService("Recycling");
+                        console.log(this.props.typeOfService);
+                      }}
+                      
+                      
+                      >Recycling</MenuItem>
+                      <MenuItem
+                        value={"NJHS Sponsored Event"}
+                      
+                        onClick={(e) => {
+                          this.props.setTypeOfService("NJHS Sponsored Event");
+                          console.log(this.props.typeOfService);
+                        }}
+                      >
+                        NJHS Sponsored Event
+                      </MenuItem>
+                      <MenuItem value={"Other"} 
+                     onClick={(e) => {
+                          this.props.setTypeOfService("Other");
+                          console.log(this.props.typeOfService);
+                        }}>
+                        Other
+                      </MenuItem>
                     </Select>
                   </FormControl>{" "}
-                  
                 </Grid>
                 <Grid item xs={12}>
                   <TextField
                     variant="outlined"
                     required
                     fullWidth
-                    name="password"
-                    label="Decription of Service"
-                    type="password"
-                    id="password"
+                    name="description"
+                    label="Description of Service"
+                    id="text"
+                    onChange={(e) => {
+                      this.props.setDescription(e.target.value);
+                      console.log(this.props.description);
+                    }}
+                    defaultValue={this.props.description}
                   />
                 </Grid>
                 <Grid item xs={12}>
-                <FormControl style={{minWidth:160, marginBottom:"25px"}} >
-                    <InputLabel id="demo-simple-select-label">Number of Hours</InputLabel>
+                  <FormControl style={{ minWidth: 160, marginBottom: "25px" }}>
+                    <InputLabel id="demo-simple-select-label">
+                      Number of Hours
+                    </InputLabel>
                     <Select
                       labelId="demo-simple-select-label"
                       id="demo-simple-select"
-                    //   value={age}
-                    //   onChange={handleChange}
+                      //   value={age}
+                      //   onChange={handleChange}
                     >
                       <MenuItem value={10}>0.5</MenuItem>
                       <MenuItem value={20}>1</MenuItem>
@@ -170,14 +202,16 @@ class AddServiceHours extends React.Component <AcceptedProps, {}> {
                   </FormControl>{" "}
                 </Grid>
               </Grid>
-              <Link to="/mydashboard"><Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="primary"
-              >
-                Submit
-              </Button></Link>
+              <Link to="/mydashboard">
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                >
+                  Submit
+                </Button>
+              </Link>
               <Grid container justify="flex-end"></Grid>
             </form>
           </div>
