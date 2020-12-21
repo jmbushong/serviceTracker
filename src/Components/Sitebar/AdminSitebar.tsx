e40f3c3cfcc29f7a065e650c94c7e9e46c96b176
Jessica Bushong <jessica.bushong08@gmail.com>
2020-12-21 11:15:22 -0600

    added dropdown menu in both sitebars

--- src/Components/Sitebar/AdminSitebar.tsx
+++ src/Components/Sitebar/AdminSitebar.tsx
@@ -7,8 +7,9 @@ import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import { Redirect } from "react-router-dom";
-
+import MenuItem from "@material-ui/core/MenuItem";
 import { Link } from "react-router-dom";
+import Menu from "@material-ui/core/Menu";
 
 type AcceptedProps = {
   backArrowToggle: any;
@@ -17,7 +18,27 @@ type AcceptedProps = {
   sessionToken: any;
 };
 
-class AdminSitebar extends React.Component<AcceptedProps, {}> {
+type myState = {
+  anchorE1: any;
+  anchorOriginVertical: any;
+  anchorOriginHorizontal: any;
+  transformOriginVertical: any;
+  transformOriginHorizontal: any;
+  anchorReference: any;
+};
+
+class AdminSitebar extends React.Component<AcceptedProps, myState> {
+  constructor(props: AcceptedProps) {
+    super(props);
+    this.state = {
+      anchorE1: null,
+      anchorOriginVertical: "bottom",
+      anchorOriginHorizontal: "right",
+      transformOriginVertical: "top",
+      transformOriginHorizontal: "right",
+      anchorReference: "anchorEl",
+    };
+  }
   
   checkForToken = () => {
     if (!this.props.sessionToken) {
@@ -28,9 +49,18 @@ class AdminSitebar extends React.Component<AcceptedProps, {}> {
   };
 
 
+  handleMenu = (e: any) => {
+    this.setState({ anchorE1: e.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorE1: null });
+  };
 
 
   render() {
+    const { anchorE1 } = this.state;
+    const open = Boolean(this.state.anchorE1);
     return (
       <React.Fragment>
         <AppBar position="static">
@@ -44,17 +74,26 @@ class AdminSitebar extends React.Component<AcceptedProps, {}> {
             ) : (
               <div></div>
             )}
-            <IconButton
-              onClick={() => {
-                this.props.clearToken();
-               
-              }}
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-            >
-              Logout
-            </IconButton>
+            <div>
+              <IconButton onClick={this.handleMenu}>
+                <MenuIcon style={{color:"white"}} />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorE1}
+                open={open}
+                onClose={this.handleClose}
+              >
+                <MenuItem
+                  onClick={() => {
+                    this.props.clearToken();
+                  }}
+                >
+                  Logout
+                </MenuItem>
+                <MenuItem onClick={this.handleClose}>Delete Account</MenuItem>
+              </Menu>
+            </div>
           </Toolbar>
         </AppBar>
         {this.checkForToken()}
--- src/Components/Sitebar/Sitebar.tsx
+++ src/Components/Sitebar/Sitebar.tsx
@@ -5,29 +5,67 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import { Link } from "react-router-dom";
-import { Redirect } from "react-router-dom"
+import { Redirect } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
 
-type AcceptedProps={
+// type myState = {
+//   anchorE1: any;
+//   setAnchorE1: (e: any) => void;
+// };
+type AcceptedProps = {
   backArrowToggle: any;
   arrowHandler: any;
   clearToken: any;
   sessionToken: any;
-}
+};
 
-class Sitebar extends React.Component<AcceptedProps, {}> {
+type myState = {
+  anchorE1: any;
+  anchorOriginVertical: any;
+  anchorOriginHorizontal: any;
+  transformOriginVertical: any;
+  transformOriginHorizontal: any;
+  anchorReference: any;
+};
+
+class Sitebar extends React.Component<AcceptedProps, myState> {
+  constructor(props: AcceptedProps) {
+    super(props);
+    this.state = {
+      anchorE1: null,
+      anchorOriginVertical: "bottom",
+      anchorOriginHorizontal: "right",
+      transformOriginVertical: "top",
+      transformOriginHorizontal: "right",
+      anchorReference: "anchorEl",
+    };
+  }
+
+
+
   checkForToken = () => {
     if (!this.props.sessionToken) {
-      return <Redirect to="/"/>;
+      return <Redirect to="/" />;
     } else {
       return console.log("hello");
     }
   };
 
+  handleMenu = (e: any) => {
+    this.setState({ anchorE1: e.currentTarget });
+  };
 
+  handleClose = () => {
+    this.setState({ anchorE1: null });
+  };
 
   render() {
+    const { anchorE1 } = this.state;
+    const open = Boolean(this.state.anchorE1);
     return (
       <React.Fragment>
         <AppBar position="static">
@@ -41,17 +79,27 @@ class Sitebar extends React.Component<AcceptedProps, {}> {
             ) : (
               <div></div>
             )}
-            <IconButton
-              onClick={() => {
-                this.props.clearToken();
-               
-              }}
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-            >
-              Logout
-            </IconButton>
+          
+            <div>
+              <IconButton onClick={this.handleMenu}>
+                <MenuIcon style={{color:"white"}} />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorE1}
+                open={open}
+                onClose={this.handleClose}
+              >
+                <MenuItem
+                  onClick={() => {
+                    this.props.clearToken();
+                  }}
+                >
+                  Logout
+                </MenuItem>
+                <MenuItem onClick={this.handleClose}>Delete Account</MenuItem>
+              </Menu>
+            </div>
           </Toolbar>
         </AppBar>
         {this.checkForToken()}
